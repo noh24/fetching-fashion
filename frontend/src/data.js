278b1b5d9313220a353c26e9934bb41b238e6d62
Brian <noh24@ymail.com>
2023-03-06 17:35:47 -0500

    add product data

---
+++ frontend/src/data.js
@@ -0,0 +1,197 @@
+const data = {
+  products: {
+    fleeceSherpas: [
+      {
+        _id: 1,
+        name: "fleece sherpa jacket",
+        color: "black",
+        countInStock: 10,
+        rating: 4.5,
+        reviews: 120,
+        images: [
+          "/images/jacket-sherpa/black-sherpa-big.png",
+          "/images/jacket-sherpa/black-sherpa-small.png",
+          "/images/jacket-sherpa/black-sherpa.png",
+        ],
+        description: [
+          'Premium human grade teddy sherpa exterior',
+          'Soft polar fleece interior',
+          'Double breasted front buttons for perfect fit',
+          'Dual buttons at the back of the neck to stabilize oversized hood',
+          'Hole opening back of the neck for leash',
+          'Ribbed knit bottom hem & sleeve cuffs',
+          'Adjustable drawstring',
+          'Perfect for indoor and outdoor wear',
+          'Machine wash cold, tumble dry cold, avoid softener'
+        ]
+      },
+      {
+        _id: 2,
+        name: "fleece sherpa jacket",
+        color: "green",
+        countInStock: 10,
+        rating: 4.5,
+        reviews: 120,
+        images: [
+          "/images/jacket-sherpa/green-sherpa-big.png",
+          "/images/jacket-sherpa/green-sherpa-small.png",
+          "/images/jacket-sherpa/green-sherpa.png",
+        ],
+        description: [
+          'Premium human grade teddy sherpa exterior',
+          'Soft polar fleece interior',
+          'Double breasted front buttons for perfect fit',
+          'Dual buttons at the back of the neck to stabilize oversized hood',
+          'Hole opening back of the neck for leash',
+          'Ribbed knit bottom hem & sleeve cuffs',
+          'Adjustable drawstring',
+          'Perfect for indoor and outdoor wear',
+          'Machine wash cold, tumble dry cold, avoid softener'
+        ]
+      },
+      {
+        _id: 3,
+        name: "fleece sherpa jacket",
+        color: "grey",
+        countInStock: 10,
+        rating: 4.5,
+        reviews: 120,
+        images: [
+          "/images/jacket-sherpa/grey-sherpa-big.png",
+          "/images/jacket-sherpa/grey-sherpa-small.png",
+          "/images/jacket-sherpa/grey-sherpa.png",
+        ],
+        description: [
+          'Premium human grade teddy sherpa exterior',
+          'Soft polar fleece interior',
+          'Double breasted front buttons for perfect fit',
+          'Dual buttons at the back of the neck to stabilize oversized hood',
+          'Hole opening back of the neck for leash',
+          'Ribbed knit bottom hem & sleeve cuffs',
+          'Adjustable drawstring',
+          'Perfect for indoor and outdoor wear',
+          'Machine wash cold, tumble dry cold, avoid softener'
+        ]
+      },
+      {
+        _id: 4,
+        name: "fleece sherpa jacket",
+        color: "maroon",
+        countInStock: 10,
+        rating: 4.5,
+        reviews: 120,
+        images: [
+          "/images/jacket-sherpa/maroon-sherpa-big.png",
+          "/images/jacket-sherpa/maroon-sherpa-small.png",
+          "/images/jacket-sherpa/maroon-sherpa.png",
+        ],
+        description: [
+          'Premium human grade teddy sherpa exterior',
+          'Soft polar fleece interior',
+          'Double breasted front buttons for perfect fit',
+          'Dual buttons at the back of the neck to stabilize oversized hood',
+          'Hole opening back of the neck for leash',
+          'Ribbed knit bottom hem & sleeve cuffs',
+          'Adjustable drawstring',
+          'Perfect for indoor and outdoor wear',
+          'Machine wash cold, tumble dry cold, avoid softener'
+        ]
+      },
+      {
+        _id: 5,
+        name: "fleece sherpa jacket",
+        color: "orange",
+        countInStock: 10,
+        rating: 4.5,
+        reviews: 120,
+        images: [
+          "/images/jacket-sherpa/orange-sherpa-big.png",
+          "/images/jacket-sherpa/orange-sherpa-small.png",
+          "/images/jacket-sherpa/orange-sherpa.png",
+        ],
+        description: [
+          'Premium human grade teddy sherpa exterior',
+          'Soft polar fleece interior',
+          'Double breasted front buttons for perfect fit',
+          'Dual buttons at the back of the neck to stabilize oversized hood',
+          'Hole opening back of the neck for leash',
+          'Ribbed knit bottom hem & sleeve cuffs',
+          'Adjustable drawstring',
+          'Perfect for indoor and outdoor wear',
+          'Machine wash cold, tumble dry cold, avoid softener'
+        ]
+      },
+    ],
+    jacketVests: [
+      {
+        _id: 1,
+        name: "parka jacket vest",
+        color: "black",
+        countInStock: 10,
+        rating: 4.5,
+        reviews: 120,
+        images: [
+          "/images/jacket-vest/black-vest-big.png",
+          "/images/jacket-vest/black-vest-small.png",
+          "/images/jacket-vest/black-vest.png",
+        ],
+        description: [
+          'Keep Dry: Water-resistant exterior shell with reflective strips on the side',
+          'Perfect Fit: Double breasted front buttons for perfect fit',
+          'Extra Warmth: Cotton insulation and polar fleece interior',
+          'Zipper hole opening for leash attachment',
+          'Pocket for poop bags and treats ',
+          'Material: Shell 100% Polyester, Filling 100% Cotton, Lining 100% Polyester',
+          'Care: Machine wash cold, hang dry, do not iron  ',
+        ]
+      },
+      {
+        _id: 2,
+        name: "parka jacket vest",
+        color: "olive",
+        countInStock: 10,
+        rating: 4.5,
+        reviews: 120,
+        images: [
+          "/images/jacket-vest/olive-vest-big.png",
+          "/images/jacket-vest/olive-vest-small.png",
+          "/images/jacket-vest/olive-vest.png",
+        ],
+        description: [
+          'Keep Dry: Water-resistant exterior shell with reflective strips on the side',
+          'Perfect Fit: Double breasted front buttons for perfect fit',
+          'Extra Warmth: Cotton insulation and polar fleece interior',
+          'Zipper hole opening for leash attachment',
+          'Pocket for poop bags and treats ',
+          'Material: Shell 100% Polyester, Filling 100% Cotton, Lining 100% Polyester',
+          'Care: Machine wash cold, hang dry, do not iron  ',
+        ]
+      },
+      {
+        _id: 3,
+        name: "parka jacket vest",
+        color: "red",
+        countInStock: 10,
+        rating: 4.5,
+        reviews: 120,
+        images: [
+          "/images/jacket-vest/red-vest-big.png",
+          "/images/jacket-vest/red-vest-small.png",
+          "/images/jacket-vest/red-vest.png",
+        ],
+        description: [
+          'Keep Dry: Water-resistant exterior shell with reflective strips on the side',
+          'Perfect Fit: Double breasted front buttons for perfect fit',
+          'Extra Warmth: Cotton insulation and polar fleece interior',
+          'Zipper hole opening for leash attachment',
+          'Pocket for poop bags and treats ',
+          'Material: Shell 100% Polyester, Filling 100% Cotton, Lining 100% Polyester',
+          'Care: Machine wash cold, hang dry, do not iron  ',
+        ]
+      },
+      
+    ]
+  },
+};
+
+export default data;
