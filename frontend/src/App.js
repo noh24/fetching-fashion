1dc504f17630b70a7d29c157efdeb5e807f780ca
Brian <noh24@ymail.com>
2023-03-08 14:02:39 -0500

    create shipping address page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import SingleWinterApparel from "./screens/SingleWinterApparel";
 import WinterApparel from "./screens/WinterApparel";
 import Cart from "./screens/Cart";
 import SignIn from "./screens/SignIn";
+import Shipping from "./screens/Shipping";
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
           <Route path='/product/:id' element={<SingleWinterApparel />} />
           <Route path='/cart' element={<Cart />} />
           <Route path='/signin' element={<SignIn />} />
+          <Route path='/shipping' element={<Shipping />} />
         </Route>
       </Routes>
     </BrowserRouter>
---
+++ frontend/src/screens/Shipping.jsx
@@ -0,0 +1,89 @@
+import React, { useContext, useEffect, useState } from "react";
+import { Helmet } from "react-helmet-async";
+import { useNavigate } from "react-router-dom";
+import { Store } from "../Store";
+
+const Shipping = () => {
+  const navigate = useNavigate();
+  const { state, dispatch: storeDispatch } = useContext(Store);
+  const { userInfo, cart, shippingAddress } = state;
+
+  const [name, setName] = useState(shippingAddress.fullName || "");
+  const [address, setAddress] = useState(shippingAddress.address || "");
+  const [city, setCity] = useState(shippingAddress.city || "");
+  const [postalCode, setPostalCode] = useState(
+    shippingAddress.postalCode || ""
+  );
+  const [country, setCountry] = useState(shippingAddress.country || "");
+
+  useEffect(() => {
+    if (cart <= 0) {
+      navigate("/");
+    }
+    if (!userInfo) {
+      navigate("/signin?redirect=/shipping");
+    }
+  }, [userInfo, navigate, cart]);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    const userShippingAddress = {
+      name,
+      address,
+      city,
+      postalCode,
+      country,
+    };
+    storeDispatch({
+      type: "SAVE_SHIPPING_ADDRESS",
+      payload: userShippingAddress,
+    });
+    localStorage.setItem("shippingAddress", userShippingAddress);
+    navigate('/payment');
+  };
+  return (
+    <main>
+      <Helmet>
+        <title>Shipping Address</title>
+      </Helmet>
+      <h1>Shipping Address</h1>
+      <form onSubmit={submitHandler}>
+        <input
+          type='text'
+          placeholder='Full Name'
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        ></input>
+        <input
+          type='text'
+          placeholder='Address'
+          value={address}
+          onChange={(e) => setAddress(e.target.value)}
+        ></input>
+        <input
+          type='text'
+          placeholder='City'
+          value={city}
+          onChange={(e) => setCity(e.target.value)}
+        ></input>
+        <input
+          type='text'
+          placeholder='Postal Code / Zip Code'
+          value={postalCode}
+          onChange={(e) => setPostalCode(e.target.value)}
+        ></input>
+        <input
+          type='text'
+          placeholder='Country'
+          value={country}
+          onChange={(e) => setCountry(e.target.value)}
+        ></input>
+        <div>
+          <button type='submit'>Proceed to Payments</button>
+        </div>
+      </form>
+    </main>
+  );
+};
+
+export default Shipping;
