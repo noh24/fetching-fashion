29d99e74a893e87dee47bf75ef9e89f8423ad0f4
Brian <noh24@ymail.com>
2023-03-08 22:31:29 -0500

    add isAuth to verify user's jwt token

--- backend/utility/token.js
+++ backend/utility/token.js
@@ -11,4 +11,21 @@ const generateToken = (user) => {
   return token;
 };
 
-export { generateToken };
+const isAuth = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (authorization) {
+    const token = authorization.slice(7, authorization.length);
+    jwt.verify(token, process.env.JWT_SECRET, (err, decode) => {
+      if (err) {
+        res.status(401).send({ message: "Invalid token" });
+      } else {
+        req.user = decode;
+        next();
+      }
+    });
+  } else {
+    res.status(401).send({ message: "No token" });
+  }
+};
+
+export { generateToken, isAuth };
