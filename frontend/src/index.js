62cf1d52470a6e37dd9adddb37a59f08962306b0
Brian <noh24@ymail.com>
2023-03-08 19:15:07 -0500

    add paypal script provider to app

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,13 +5,16 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { HelmetProvider } from "react-helmet-async";
 import { StoreProvider } from "./Store";
+import { PayPalScriptProvider } from "@paypal/react-paypal-js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <HelmetProvider>
       <StoreProvider>
-        <App />
+        <PayPalScriptProvider deferLoading={true}>
+          <App />
+        </PayPalScriptProvider>
       </StoreProvider>
     </HelmetProvider>
   </React.StrictMode>
