5787ef3d67e3b189efeaf597d8d795fbfe19e175
Brian <noh24@ymail.com>
2023-03-09 23:07:16 -0500

    add infinite scrolling carousel

--- frontend/src/screens/Home.jsx
+++ frontend/src/screens/Home.jsx
@@ -1,18 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import FeaturedHomeSection from "./../components/FeaturedHomeSection";
 import { Helmet } from "react-helmet-async";
 
 const Home = () => {
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const slideShow = [
+    "/images/home-display1.png",
+    "/images/home-display2.png",
+    "/images/home-display3.png",
+  ];
+  const carouselInfiniteScroll = () => {
+    if (currentSlide === slideShow.length - 1) {
+      return setCurrentSlide(0);
+    }
+    return setCurrentSlide(currentSlide + 1);
+  };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      carouselInfiniteScroll();
+    }, 3000);
+    return () => clearInterval(interval);
+  });
+
   return (
     <main>
       <Helmet>
         <title>Home</title>
       </Helmet>
-      <section>
-        <img src='/images/home-display1.png' alt='home-display1' />
-        <img src='/images/home-display2.png' alt='home-display2' />
-        <img src='/images/home-display3.png' alt='home-display3' />
+      <section className='relative overflow-hidden flex-nowrap flex'>
+        {slideShow.map((slide, index) => (
+          <img
+            className='min-w-full sm:h-screen object-cover w-full transition duration-1000 flex items-center justify-center ease-in'
+            style={{ transform: `translate(-${currentSlide * 100}%)` }}
+            src={slide}
+            alt='dog wearing clothes'
+            key={slide}
+          ></img>
+        ))}
       </section>
 
       <section>
