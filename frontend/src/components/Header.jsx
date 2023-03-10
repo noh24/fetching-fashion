a99998fb4c3308f2add484cb63ad40898807a89a
Brian <noh24@ymail.com>
2023-03-10 17:33:27 -0500

    add nav bar drop down menu

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,13 +1,14 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useRef, useState } from "react";
 import { NavLink, useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import { Store } from "../Store";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
 
 const Header = () => {
   const navigate = useNavigate();
   const { state, dispatch: storeDispatch } = useContext(Store);
   const { cart, userInfo } = state;
-
+  const [openMenu, setOpenMenu] = useState(false);
   const signOutHandler = () => {
     storeDispatch({ type: "USER_SIGNOUT" });
     localStorage.removeItem("cart");
@@ -18,47 +19,71 @@ const Header = () => {
     navigate("/signin");
   };
 
+  const dropMenu = useRef(null);
+  
+  const handleMouseDown = (e) => {
+    if (dropMenu.current && !dropMenu.current.contains(e.target)) {
+      setOpenMenu(false);
+    }
+  };
+  document.addEventListener("mousedown", handleMouseDown);
+
   return (
-    <nav>
-      <NavLink to='/'>
-        <img
-          src='/images/fetching-fashion-logo.png'
-          alt='fetching fashion logo'
-        />
-      </NavLink>
-      <div>
-        <NavLink to='/'>Home</NavLink>
+    <nav className='container flex items-center justify-between px-4 font-medium'>
+      <div className='w-40'>
+        <NavLink to='/'>
+          <img
+            src='/images/fetching-fashion-logo.png'
+            alt='fetching fashion logo'
+          />
+        </NavLink>
       </div>
-      <div>
-        <NavLink to='/products/winter-apparel'>Winter Apparel</NavLink>
-      </div>
-      <div>
-        <NavLink to='/cart'>Cart</NavLink>
-        <span>
-          {cart.length > 0 &&
-            cart.reduce((acc, curr) => acc + curr.quantity, 0)}
-        </span>
-      </div>
-      <div>
-        {userInfo ? (
-          <div>
-            <p>{userInfo.name}</p>
-            <p>
-              <NavLink to='/profile'>Profile</NavLink>
-            </p>
-            <p>
-              <NavLink to='/orderhistory'>Order History</NavLink>
-            </p>
-            <p>
-              <button onClick={signOutHandler}>Sign out</button>
-            </p>
+
+      <section className='flex justify-center items-center gap-2 relative'>
+        <div>
+          <NavLink to='/cart'>Cart</NavLink>
+          <span>
+            {cart.length > 0 &&
+              cart.reduce((acc, curr) => acc + curr.quantity, 0)}
+          </span>
+        </div>
+
+        <div>
+          <div className=''>
+            <NavLink to='/products/winter-apparel'>Products</NavLink>
           </div>
-        ) : (
-          <NavLink to='/signin'>Sign In</NavLink>
-        )}
-      </div>
-      <div></div>
-      <div></div>
+        </div>
+
+        <div
+          className='flex cursor-pointer items-center'
+          onClick={userInfo ? () => setOpenMenu((prev) => !prev) : null}
+        >
+          {userInfo ? (
+            <p>
+              {userInfo.name}
+              <ArrowDropDownIcon fontSize='small' />
+            </p>
+          ) : (
+            <NavLink to='/signin'>Sign In</NavLink>
+          )}
+        </div>
+
+        <div
+          ref={dropMenu}
+          className={
+            openMenu
+              ? "absolute flex flex-col z-20 top-10 left-2 bg-neutral-100 min-w-full text-right px-2 py-2 rounded space-y-1"
+              : "hidden"
+          }
+        >
+          <NavLink to={"/profile"}>User Profile</NavLink>
+          <NavLink to={"/orderhistory"}>Order History</NavLink>
+          <hr />
+          <NavLink to={"#signout"} onClick={signOutHandler}>
+            Sign Out
+          </NavLink>
+        </div>
+      </section>
     </nav>
   );
 };
