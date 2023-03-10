e369e19842be8b1c2855ca900ab8ddb78773dc8c
Brian <noh24@ymail.com>
2023-03-10 16:18:11 -0500

    remove outline from css

--- frontend/src/components/SharedLayout.jsx
+++ frontend/src/components/SharedLayout.jsx
@@ -7,7 +7,7 @@ import "react-toastify/dist/ReactToastify.css";
 
 const SharedLayout = () => {
   return (
-    <div className='container mx-auto flex flex-col justify-between items-center outline min-h-screen w-full'>
+    <div className='container mx-auto flex flex-col justify-between items-center min-h-screen w-full'>
       <ToastContainer position='bottom-center' index={1} />
       <Header />
       <Outlet />
