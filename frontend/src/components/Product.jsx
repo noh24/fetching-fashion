b239db289a0b811224a3cb8f500d4b3d0c019606
Brian <noh24@ymail.com>
2023-03-11 18:03:59 -0500

    add text center on products

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -24,7 +24,7 @@ const Product = (props) => {
   };
 
   return (
-    <div className='flex flex-col justify-center items-center space-y-4 my-6'>
+    <div className='flex flex-col justify-center items-center space-y-4 my-6 text-center'>
       <Link to={`/product/${product._id}`}>
         <div className='h-[400px] w-full sm:w-[400px] overflow-hidden relative mb-4'>
           <img
