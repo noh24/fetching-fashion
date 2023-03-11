78b83dd014746f684411f2fe18c0b4d8150db493
Brian <noh24@ymail.com>
2023-03-10 20:52:30 -0500

    add headless ui menu drop down

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,14 +1,18 @@
-import React, { useContext, useEffect, useRef, useState } from "react";
+import React, { Fragment, useContext } from "react";
 import { NavLink, useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import { Store } from "../Store";
-import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+import { Menu, Transition } from "@headlessui/react";
+import ShoppingBagOutlinedIcon from "@mui/icons-material/ShoppingBagOutlined";
+import KeyboardArrowDownOutlinedIcon from "@mui/icons-material/KeyboardArrowDownOutlined";
+import PersonIcon from "@mui/icons-material/Person";
+import MenuIcon from "@mui/icons-material/Menu";
 
 const Header = () => {
   const navigate = useNavigate();
   const { state, dispatch: storeDispatch } = useContext(Store);
   const { cart, userInfo } = state;
-  const [openMenu, setOpenMenu] = useState(false);
+
   const signOutHandler = () => {
     storeDispatch({ type: "USER_SIGNOUT" });
     localStorage.removeItem("cart");
@@ -18,22 +22,55 @@ const Header = () => {
     toast.info("Successfully logged out");
     navigate("/signin");
   };
-
-  const dropMenu = useRef(null);
-  
-  const handleMouseDown = (e) => {
-    if (dropMenu.current && !dropMenu.current.contains(e.target)) {
-      setOpenMenu(false);
-    }
+  const routeTo = (path) => {
+    navigate(path);
   };
-  document.addEventListener("mousedown", handleMouseDown);
 
   return (
-    <nav className='container flex items-center justify-between px-4 font-medium'>
-      <div className='w-40'>
+    <nav className='container flex items-center justify-between py-3 sm:py-4 px-2 font-medium'>
+      <div className=''>
+        <Menu as='div' className='relative inline-block text-left z-10'>
+          <div>
+            <Menu.Button className='inline-flex w-full justify-center items-center rounded-md font-medium focus:outline-none capitalize'>
+              <div className=''>
+                <MenuIcon fontSize='medium' />
+              </div>
+            </Menu.Button>
+          </div>
+          <Transition
+            as={Fragment}
+            enter='transition ease-out duration-100'
+            enterFrom='transform opacity-0 scale-95'
+            enterTo='transform opacity-100 scale-100'
+            leave='transition ease-in duration-75'
+            leaveFrom='transform opacity-100 scale-100'
+            leaveTo='transform opacity-0 scale-95'
+          >
+            <Menu.Items className='absolute mt-2 sm:-mr-1 mr-1 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none'>
+              <div className='px-1 py-1 '>
+                <Menu.Item>
+                  {({ active }) => (
+                    <button
+                      onClick={() => routeTo("/products/winter-apparel")}
+                      className={`${
+                        active ? "bg-gray-800 text-white" : "text-gray-900"
+                      } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                    >
+                      Winter Apparel
+                    </button>
+                  )}
+                </Menu.Item>
+              </div>
+            </Menu.Items>
+          </Transition>
+        </Menu>
+      </div>
+
+      <div className='w-28 sm:w-40'>
         <NavLink to='/'>
           <img
-            src='/images/fetching-fashion-logo.png'
+            className='object-cover'
+            src='/images/fetching-fashion-1.png'
             alt='fetching fashion logo'
           />
         </NavLink>
@@ -41,48 +78,105 @@ const Header = () => {
 
       <section className='flex justify-center items-center gap-2 relative'>
         <div>
-          <NavLink to='/cart'>Cart</NavLink>
+          <div className=''>
+            <NavLink to='/cart'>
+              <ShoppingBagOutlinedIcon fontSize='medium' />
+            </NavLink>
+          </div>
           <span>
             {cart.length > 0 &&
               cart.reduce((acc, curr) => acc + curr.quantity, 0)}
           </span>
         </div>
 
-        <div>
-          <div className=''>
-            <NavLink to='/products/winter-apparel'>Products</NavLink>
+        <Menu as='div' className='relative inline-block text-left z-10'>
+          <div>
+            <Menu.Button className='inline-flex w-full justify-center items-center rounded-md font-medium focus:outline-none capitalize'>
+              <div className=''>
+                <PersonIcon fontSize='medium' />
+              </div>
+              {userInfo && (
+                <div>
+                  <KeyboardArrowDownOutlinedIcon
+                    fontSize='small'
+                    aria-hidden='true'
+                  />
+                </div>
+              )}
+            </Menu.Button>
           </div>
-        </div>
-
-        <div
-          className='flex cursor-pointer items-center'
-          onClick={userInfo ? () => setOpenMenu((prev) => !prev) : null}
-        >
-          {userInfo ? (
-            <p>
-              {userInfo.name}
-              <ArrowDropDownIcon fontSize='small' />
-            </p>
-          ) : (
-            <NavLink to='/signin'>Sign In</NavLink>
-          )}
-        </div>
-
-        <div
-          ref={dropMenu}
-          className={
-            openMenu
-              ? "absolute flex flex-col z-20 top-10 left-2 bg-neutral-100 min-w-full text-right px-2 py-2 rounded space-y-1"
-              : "hidden"
-          }
-        >
-          <NavLink to={"/profile"}>User Profile</NavLink>
-          <NavLink to={"/orderhistory"}>Order History</NavLink>
-          <hr />
-          <NavLink to={"#signout"} onClick={signOutHandler}>
-            Sign Out
-          </NavLink>
-        </div>
+          <Transition
+            as={Fragment}
+            enter='transition ease-out duration-100'
+            enterFrom='transform opacity-0 scale-95'
+            enterTo='transform opacity-100 scale-100'
+            leave='transition ease-in duration-75'
+            leaveFrom='transform opacity-100 scale-100'
+            leaveTo='transform opacity-0 scale-95'
+          >
+            {userInfo ? (
+              <Menu.Items className='absolute right-0 mt-2 sm:-mr-1 mr-1 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none'>
+                <div className='px-1 py-1 '>
+                  <Menu.Item>
+                    {({ active }) => (
+                      <button
+                        onClick={() => routeTo("/profile")}
+                        className={`${
+                          active ? "bg-gray-800 text-white" : "text-gray-900"
+                        } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                      >
+                        User Profile
+                      </button>
+                    )}
+                  </Menu.Item>
+                  <Menu.Item>
+                    {({ active }) => (
+                      <button
+                        onClick={() => routeTo("/orderhistory")}
+                        className={`${
+                          active ? "bg-gray-800 text-white" : "text-gray-900"
+                        } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                      >
+                        Order History
+                      </button>
+                    )}
+                  </Menu.Item>
+                </div>
+                <div className='px-1 py-1'>
+                  <Menu.Item>
+                    {({ active }) => (
+                      <button
+                        onClick={signOutHandler}
+                        className={`${
+                          active ? "bg-gray-800 text-white" : "text-gray-900"
+                        } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                      >
+                        Sign Out
+                      </button>
+                    )}
+                  </Menu.Item>
+                </div>
+              </Menu.Items>
+            ) : (
+              <Menu.Items className='absolute right-0 mt-2 sm:-mr-1 mr-1 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none'>
+                <div className='px-1 py-1'>
+                  <Menu.Item>
+                    {({ active }) => (
+                      <button
+                        onClick={() => routeTo("/signin")}
+                        className={`${
+                          active ? "bg-gray-800 text-white" : "text-gray-900"
+                        } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                      >
+                        Sign In
+                      </button>
+                    )}
+                  </Menu.Item>
+                </div>
+              </Menu.Items>
+            )}
+          </Transition>
+        </Menu>
       </section>
     </nav>
   );
