b52114ba683d003ba674a064d31a3c4e39d24c76
Brian <noh24@ymail.com>
2023-03-11 15:50:54 -0500

    add hover effect and css to products

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -7,7 +7,7 @@ import axios from "axios";
 
 const Product = (props) => {
   const { product } = props;
-  const { state, dispatch : storeDispatch } = useContext(Store);
+  const { state, dispatch: storeDispatch } = useContext(Store);
 
   const addCartHandler = async (product) => {
     try {
@@ -17,27 +17,46 @@ const Product = (props) => {
       if (data.countInStock < quantity) {
         throw new Error(`${itemExist.name || product.name} is out of stock.`);
       }
-      storeDispatch({ type: "ADD_TO_CART", payload: {...product, quantity} })
+      storeDispatch({ type: "ADD_TO_CART", payload: { ...product, quantity } });
     } catch (err) {
       toast.error(getError(err));
     }
   };
 
   return (
-    <div>
+    <div className='flex flex-col justify-center items-center space-y-4'>
       <Link to={`/product/${product._id}`}>
-        <img src={product.images[0]} alt={`${product.color} ${product.name}`} />
-        <img src={product.images[1]} alt={`${product.color} ${product.name}`} />
-        <p>{`${product.color} ${product.name}`}</p>
-        <div>
+        <div className='h-[400px] w-full sm:w-[400px] overflow-hidden relative mb-4'>
+          <img
+            className='h-full w-full object-cover z-10 hover:z-0 sm:absolute'
+            src={product.images[0]}
+            alt={`${product.color} ${product.name}`}
+          />
+          <img
+            className='h-full w-full object-cover hover:z-10 sm:absolute'
+            src={product.images[1]}
+            alt={`${product.color} ${product.name}`}
+          />
+        </div>
+
+        <div className='space-y-3 font-medium '>
+          <p className='text-lg'>{`${product.color} ${product.name}`}</p>
           <p>
-            <span>${product.price + 10}</span> ${product.price}
+            <span className='line-through text-gray-400'>${product.price + 10}</span>{" "}
+            <span className='text-green-500'>${product.price}</span>
           </p>
         </div>
       </Link>
-      <button type='button' onClick={() => addCartHandler(product)}>
-        Add To Cart
-      </button>
+
+      <div>
+        <button
+          className='bg-gray-800 text-gray-200 font-medium px-4 py-2 rounded-full shadow-sm hover:rounded-full'
+          type='button'
+          onClick={() => addCartHandler(product)}
+        >
+          Add To Cart
+        </button>
+      </div>
     </div>
   );
 };
