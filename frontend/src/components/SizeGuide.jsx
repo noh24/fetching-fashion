5a3e9d9d741b7eeb0fc017f7000f6da5ced0b930
Brian <noh24@ymail.com>
2023-03-11 20:28:51 -0500

    add flex layout & spacing to size guide

--- frontend/src/components/SizeGuide.jsx
+++ frontend/src/components/SizeGuide.jsx
@@ -1,20 +1,38 @@
-import React from 'react'
+import React from "react";
 
 const SizeGuide = () => {
   return (
-    <section>
-      <div>
-        <p>Back Length</p>
-        <p>Measure from the base of your dogs neck (right below where their collar would sit) all the way to just above the top of their tail.</p>
-      <img src="/images/back-length.png" alt="back-length-guide" />
+    <section className='px-2 space-y-4 my-32'>
+      <div className='flex justify-between space-y-4 space-x-4'>
+        <div className='flex flex-col text-sm text-justify space-y-4'>
+          <p className='uppercase font-medium'>Back Length</p>
+          <p>
+            Measure from the base of your dogs neck (right below where their
+            collar would sit) all the way to just above the top of their tail.
+          </p>
+        </div>
+        <img
+          src='/images/back-length.png'
+          alt='back-length-guide'
+          className='w-28 '
+        />
       </div>
-      <div>
-        <p>CHEST GIRTH</p>
-        <p>Measure around the largest part of your dogs chest, just behind their front legs.</p>
-      <img src="/images/chest-girth.png" alt="chest-girth-guide" />
+      <div className='flex justify-between space-y-4 space-x-4 w-full'>
+        <div className='flex flex-col text-sm text-justify space-y-4'>
+          <p className='uppercase font-medium'>CHEST GIRTH</p>
+          <p>
+          Measure around the largest part of your dogs chest, just behind their
+          front legs.
+          </p>
+        </div>
+        <img
+          src='/images/chest-girth.png'
+          alt='/images/chest-girth.png'
+          className='w-28'
+        />
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default SizeGuide
+export default SizeGuide;
