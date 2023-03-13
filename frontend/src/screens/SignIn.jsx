dd3b31a2b654f9d79ff307b1cd292e7bee4f5c4f
Brian <noh24@ymail.com>
2023-03-13 13:44:08 -0400

    add styling and format layout

--- frontend/src/screens/SignIn.jsx
+++ frontend/src/screens/SignIn.jsx
@@ -12,7 +12,7 @@ const SignIn = () => {
   const redirect = redirectInUrl ? redirectInUrl : "/";
 
   const navigate = useNavigate();
-  const { state, dispatch : storeDispatch } = useContext(Store);
+  const { state, dispatch: storeDispatch } = useContext(Store);
   const { userInfo } = state;
 
   const [email, setEmail] = useState("");
@@ -34,48 +34,59 @@ const SignIn = () => {
       storeDispatch({ type: "USER_SIGNIN", payload: data });
       localStorage.setItem("userInfo", JSON.stringify(data));
       navigate(redirect);
-      toast.success('Successfully sign in');
+      toast.success("Successfully sign in");
     } catch (err) {
       toast.error(getError(err));
     }
   };
 
   const demoAccount = () => {
-    setEmail('demo@example.com');
-    setPassword('123456');
-  }
+    setEmail("demo@example.com");
+    setPassword("123456");
+  };
 
   return (
-    <main>
+    <main className='py-4 px-2 space-y-4 flex flex-col'>
       <Helmet>
         <title>Sign In</title>
       </Helmet>
-      <h1>Sign In</h1>
-      <section>
-        <form onSubmit={submitHandler}>
-          <input
-            type='email'
-            placeholder='Email Address'
-            required
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <input
-            type='password'
-            placeholder='Password'
-            required
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <div>
-            <button type='submit'>Sign In</button>
-            <button type='submit' onClick={demoAccount}>Demo Account</button>
-          </div>
-        </form>
-        <p>
-          New Customer? <Link to='/signup?redirect=/shipping'>Sign Up</Link>
-        </p>
-      </section>
+      <h1 className='text-2xl font-medium'>Sign In</h1>
+      <form onSubmit={submitHandler} className='space-y-4'>
+        <input
+          className='py-2 px-3 w-full border rounded-full focus:border-none focus:outline-none'
+          type='email'
+          placeholder='Email Address'
+          required
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          className='py-2 px-3 w-full border rounded-full focus:border-none focus:outline-none'
+          type='password'
+          placeholder='Password'
+          required
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <div className='flex gap-4'>
+          <button
+            className='bg-gray-800 text-white text-sm font-medium px-8 py-3 rounded-full w-full shadow-sm hover:opacity-90'
+            type='submit'
+          >
+            Sign In
+          </button>
+          <button
+            className='bg-gray-800 text-white text-sm font-medium px-8 py-3 rounded-full w-full shadow-sm hover:opacity-90'
+            type='submit'
+            onClick={demoAccount}
+          >
+            Demo Account
+          </button>
+        </div>
+      </form>
+      <p>
+        New Customer? <Link to='/signup?redirect=/shipping' className='underline text-gray-800'>Sign Up</Link>
+      </p>
     </main>
   );
 };
