5e2465eb5fdf12eb6297d301c9fa164eb1b53195
Brian <noh24@ymail.com>
2023-03-13 14:47:18 -0400

    add container and flex to sharedlayout outlets

--- frontend/src/components/SharedLayout.jsx
+++ frontend/src/components/SharedLayout.jsx
@@ -10,7 +10,7 @@ const SharedLayout = () => {
     <div className='container mx-auto flex flex-col justify-between items-center min-h-screen w-full relative text-gray-500'>
       <ToastContainer position='bottom-center' index={1} />
       <Header />
-      <div className='flex-1'>
+      <div className='flex-1 w-full container mx-auto flex justify-center'>
         <Outlet />
       </div>
       <Footer />
