e8e9dd40b885ee48284d1b2cb6e9e73948a51b6b
Brian <noh24@ymail.com>
2023-03-13 13:18:26 -0400

    add table structure to size table

--- frontend/src/components/SizeTable.jsx
+++ frontend/src/components/SizeTable.jsx
@@ -4,21 +4,21 @@ import sizingData from '../data/sizingData';
 const SizeTable
  = () => {
   return (
-    <section>
-        <h1>Size by Measurements</h1>
-        <table>
+    <section className='my-24 flex flex-col items-center max-w-2xl w-full font-light'>
+        <h1 className='text-2xl mb-12'>Size by Measurements</h1>
+        <table className='table-fixed text-center w-full'>
           <thead>
-            <tr>
-              <th>Size</th>
-              <th>Chest</th>
-              <th>Neck</th>
-              <th>Back</th>
+            <tr className='uppercase text-sm border-b'>
+              <th className='py-4 px-2'>Size: Inches</th>
+              <th className='py-4 px-2'>Chest</th>
+              <th className='py-4 px-2'>Neck</th>
+              <th className='py-4 px-2'>Back</th>
             </tr>
           </thead>
           <tbody>
             {sizingData.map(size => (
-              <tr key={size.size}>
-                <td>{size.size}</td>
+              <tr key={size.size} className='hover:bg-gray-200'>
+                <td className='py-2 px-2 font-medium'>{size.size}</td>
                 <td>{size.chest}</td>
                 <td>{size.neck}</td>
                 <td>{size.back}</td>
