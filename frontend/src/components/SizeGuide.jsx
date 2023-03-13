6b2a2eccbe6a6ab0663964d51aa735f0f26d6d1d
Brian <noh24@ymail.com>
2023-03-13 13:07:47 -0400

    change color and spacing

--- frontend/src/components/SizeGuide.jsx
+++ frontend/src/components/SizeGuide.jsx
@@ -2,13 +2,18 @@ import React from "react";
 
 const SizeGuide = () => {
   return (
-    <section className='px-2 space-y-4 my-32'>
+    <section className='px-2 space-y-4 mb-24'>
       <div className='flex justify-between space-y-4 space-x-4'>
         <div className='flex flex-col text-sm text-justify space-y-4'>
-          <p className='uppercase font-medium'>Back Length</p>
-          <p>
-            Measure from the base of your dogs neck (right below where their
-            collar would sit) all the way to just above the top of their tail.
+          <p className='uppercase font-medium text-gray-500'>
+            Back Length
+          </p>
+          <p className='text-gray-800'>
+            Measure from the base of your dogs neck{" "}
+            <span className='text-gray-500 italic font-light'>
+              right below where their collar would sit
+            </span>{" "}
+            all the way to just above the top of their tail.
           </p>
         </div>
         <img
@@ -19,10 +24,12 @@ const SizeGuide = () => {
       </div>
       <div className='flex justify-between space-y-4 space-x-4 w-full'>
         <div className='flex flex-col text-sm text-justify space-y-4'>
-          <p className='uppercase font-medium'>CHEST GIRTH</p>
-          <p>
-          Measure around the largest part of your dogs chest, just behind their
-          front legs.
+          <p className='uppercase font-medium text-gray-500'>
+            CHEST GIRTH
+          </p>
+          <p className='text-gray-800'>
+            Measure around the largest part of your dogs chest, just behind
+            their front legs.
           </p>
         </div>
         <img
