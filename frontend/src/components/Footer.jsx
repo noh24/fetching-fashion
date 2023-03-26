4fdbacb7b8823336875f6f4926bb60e6abdcbc21
Brian <noh24@ymail.com>
2023-03-25 21:13:30 -0400

    change layout of footer by removing fixed height

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -17,12 +17,12 @@ const Footer = () => {
     }
   };
   return (
-    <section className='w-full h-56 px-2 flex flex-col bg-gray-800 text-white lg:pt-4 lg:pb-1'>
-      <article className='lg:grid lg:grid-cols-7 lg:grid-rows-2 flex flex-col justify-between items-center h-full space-y-8 lg:space-y-0'>
+    <section className='w-full px-2 flex flex-col bg-gray-800 text-white py-4 lg:py-12'>
+      <article className='lg:grid lg:grid-cols-7 lg:grid-rows- flex flex-col justify-between items-center h-full space-y-8 lg:space-y-0'>
         {/* LOGO */}
-        <div className='xl:col-start-2 lg:col-span-2 row-span-2 flex flex-col justify-between items-center h-full'>
+        <div className='xl:col-start-2 lg:col-span-2 row-span-2 flex flex-col justify-between items-center h-full pt-4'>
           <img
-            className='object-cover w-96 invert'
+            className='object-cover w-56 invert'
             src='/images/fetching-fashion-1.png'
             alt='fetching fashion logo'
           />
@@ -49,18 +49,18 @@ const Footer = () => {
         </div>
         {/* CTA */}
         <div className='lg:col-span-2 flex flex-col'>
-          <h1 className='font-medium text-lg text-gray-800 lg:text-white'>Let's Stay Connected</h1>
-          <p className='text-gray-600 lg:text-gray-400'>Enter your email to unlock 10% OFF.</p>
+          <h1 className='font-medium text-lg text-white'>Let's Stay Connected</h1>
+          <p className='text-gray-400'>Enter your email to unlock 10% OFF.</p>
           <form onSubmit={submitHandler} className='mt-4 flex-nowrap flex flex-auto'>
             <input
               type='email'
               value={email}
               onChange={(e) => setEmail(e.target.value)}
               placeholder='Enter your email-address'
-              className='py-2 px-4 border-2 border-gray-300 rounded-full rounded-r-none focus:outline-none'
+              className='py-2 px-4 border border-gray-300 rounded-full rounded-r-none focus:outline-none'
             />
             <button
-              className='lg:shadow lg:shadow-gray-600 rounded-full rounded-l-none py-2 px-4 bg-gray-800 text-white border-2 border-gray-600 focus:outline-none hover:opacity-80'
+              className=' shadow-sm shadow-gray-600 border-t-2 rounded-full rounded-l-none py-2 px-4 bg-gray-800 text-white border border-gray-600 focus:outline-none hover:opacity-80'
               type='submit'
             >
               Subscribe
@@ -68,7 +68,7 @@ const Footer = () => {
           </form>
         </div>
         {/* COPYRIGHT */}
-        <small className='lg:hidden text-gray-800'>
+        <small className='lg:hidden'>
           Copyright &copy; 2023. All rights reserved.{" "}
         </small>
       </article>
